3ab2155eefd5a1ee83cc5b7ee6eb7abab01d9a17
Wuzuy <ffxtreme9@gmail.com>
2024-06-08 22:50:35 -0300

    Final Version

--- src/uec/Luta.java
+++ src/uec/Luta.java
@@ -1,6 +1,93 @@
 
 package uec;
 
+import java.util.Random;
+
 public class Luta {
+    //Atributos
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int rounds;
+    private boolean aprovada;
+    
+    //métodos
+    public void marcarLuta(Lutador l1, Lutador l2){
+        if (l1.getCategoria().equals(l2.getCategoria()) && l1 != l2){
+            this.setAprovada(true);
+            desafiado = l1;
+            desafiante = l2;
+        }else{
+            this.setAprovada(false);
+            this.setDesafiado(null);
+            this.setDesafiante(null);
+        }
+    }
+    public void lutar(){
+        if (this.aprovada) {
+            System.out.println("### DESAFIANTE ###");
+            this.desafiante.apresentar();
+            System.out.println("");
+            System.out.println("### DESAFIADO ###");
+            this.desafiante.apresentar();
+            
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3); //(0, 1 ou 2)
+            switch(vencedor) {
+                case 0 -> {
+                    //empate
+                    System.out.println("Empatou!");
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                }
+                case 1 -> {
+                    // Desafiado winner
+                    System.out.println("Vitoria do " + this.desafiado.getNome());
+                    this.desafiado.ganharLuta();
+                    this.desafiante.perderLuta();
+                }
+                case 2 -> {
+                    // Desafiante winner
+                    System.out.println("Vitoria do " + this.desafiante.getNome());
+                    this.desafiado.perderLuta();
+                    this.desafiante.ganharLuta();
+                }
+            }
+        }else {
+            System.out.println("A luta não pode acontecer.");
+        }
+    }
+    //métodos especiais
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
     
 }
--- src/uec/UEC.java
+++ src/uec/UEC.java
@@ -24,7 +24,15 @@ public class UEC {
         l[4] = new Lutador("UF0Cobol", "Brasil", 37, 1.70f, 119.3f, 5, 4, 3);
         l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f, 12, 2, 4);
         
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(l[2], l[3]);
+        System.out.println("=====================================");
+        UEC01.lutar();
+        System.out.println("======================================");
+        l[2].status();
+        System.out.println("");
         l[3].status();
+        
     }
     
 }
